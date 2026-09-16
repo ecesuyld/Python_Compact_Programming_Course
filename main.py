688b017b7ff8af7ca03fc006e62dd7f3626797cb
Ecesu <ecesu.yildiran@sdt.antalya.edu.tr>
2026-09-16 19:42:06 +0300

    Week 1- Task1

---
+++ main.py
@@ -0,0 +1,5 @@
+from math import pi
+
+if __name__ == "__main__":
+    r=float(input("Enter the radius of the circle: "))
+    print("The area of the circle with radius", str(r), "is: ", str(pi*r**2))
