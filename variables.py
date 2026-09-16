8941f080f1b491e43789a41b4d0cf878280bee8c
Ecesu <ecesu.yildiran@sdt.antalya.edu.tr>
2026-09-16 20:12:43 +0300

    Week 1- Task2

---
+++ variables.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+
+    zahl = 10
+    kommazahl = 10.5
+    text = "Hello, World!"
+    wahrheitswert = True
+
+    print(f"{zahl} is an integer, Type: {type(zahl)}")
+    print(f"{kommazahl} is a float, Type: {type(kommazahl)}")
+    print(f"{text} is a string, Type: {type(text)}")
+    print(f"{wahrheitswert} is a boolean, Type: {type(wahrheitswert)}")   
+    
+
